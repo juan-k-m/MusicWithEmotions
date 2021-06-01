e249a4fb8c8ec96d54625a8cde1e65a90964b6c5
Juan Mayoral <mayoral@juank.de>
2021-06-01 16:27:05 +0200

    musicgeneration: add functionality

--- MusicWithEmotions/musicgeneration.py
+++ MusicWithEmotions/musicgeneration.py
@@ -1,4 +1,4 @@
-from MusicWithEmotions.services import Music
+from MusicWithEmotions.services.music import Music
 
 '''Class to process the input from the encoder part and
 generate the music composition '''
@@ -6,7 +6,7 @@ class Musicgeneration:
     def __init__(self):
         self.music_helper = Music()
     def get_notes_from_emotion(self, emotion):
-        notes = self.music_helper.get_modes_to_chords()
+        return self.music_helper.get_modes_to_chords()
     
 if __name__ == '__main__':
 	test = Musicgeneration()
