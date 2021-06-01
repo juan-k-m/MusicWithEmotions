f70c724ac0c9e42a8da85aafec4cc8849c1a5243
Juan Mayoral <mayoral@juank.de>
2021-06-01 16:22:14 +0200

    musicgeneration: add functionality

--- api/encoder_endpoint.py
+++ api/encoder_endpoint.py
@@ -18,7 +18,20 @@ app.add_middleware(
 
 # http://127.0.0.1:8000/predict?pickup_datetime=2012-10-06 12:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2
 
-
 @app.get("/")
 def index():
     return dict(greeting="hello")
+
+#retrieve image from the user
+
+#call the module Face recognition -> make the prediction -> happy
+
+#encoder module is going to tranform concept into musical notes -> one bar (5-6 notes)
+
+#music generation is going to load the model and pass the notes
+
+#model creates the samll piece of music and returns a midi file
+
+
+
+
