60d7e95fa9fd3e5f127ccffc3f1373f07783e18b
Juan Mayoral <mayoral@juank.de>
2021-06-02 11:25:41 +0200

    creating programm workflow

--- ui/explore_streamlit.py
+++ ui/explore_streamlit.py
@@ -34,6 +34,11 @@ st.markdown("""
 
 """)
 
+##### Initialize flags
+flag = False
+flag_call_program = False
+our_test = None
+flag_play = False
 
 # And within an expander
 my_expander = st.beta_expander("Upload a foto...", expanded=False)
@@ -44,88 +49,23 @@ with my_expander:
 
 if uploaded_file is not None:
     #TODO clean the input from the userxc
-    st.write(type(uploaded_file))
-    st.write(uploaded_file.__dict__)
     image = Image.open(uploaded_file)
-    st.image(image, caption='Your Image.', width=500,  use_column_width=False)
-    st.write("Recognizing emotion...")
-   
+    st.image(image, caption='Your Image.', width=100,  use_column_width=False)
+    flag = True
     
-
-    # fake algorithm
-    #@st.cache
-    random_number = random.randint(0, 7)
-    your_emotion = emotions_list[random_number]
-    #your_emotion = predict(uploaded_file)
-
-    st.write(f'i see you feel {your_emotion}')
-
-# into sidebar for elements
-# add_selectbox = st.sidebar.selectbox(
-#     "'choose instruments'",
-#     ('Guitar', 'Drums', 'Piano', 'Violin')
-# )
-
-instruments = st.sidebar.multiselect("choose instrument", 
-        ('Guitar', 'Drums', 'Piano', 'Violin')
-        )
-
-# instruments = ['Guitar', 'Drums', 'Piano', 'Violin']
-# options = st.multiselect(
-#    'choose instruments',
-#    instruments,
-#    instruments[:2]
-#    )
-#st.write('You selected:', options)
-
-if st.button('Create music from foto!'):
-    our_test = test.get_notes_from_emotion('happy')
-    st.write(our_test)
+if flag is True:
+    st.button('Create music from foto!')
+    flag_call_program = True
     #st.balloons()
 
-if st.button('Play music from foto!'):
+if flag_call_program is True:
+   our_test = test.get_notes_from_emotion('happy')
+   #returns a MIDI file
+   flag_play = True
+  
+if flag_play is True:
+    st.button('Play music from foto!')
     audio_file = open('chopin.ogg', 'rb')
     audio_bytes = audio_file.read()
     st.audio(audio_bytes, format='audio/ogg')
-
-
-# select your emotional state without foto
-
-st.markdown("""
-##
-## You don't want to deal with pictures?
-#### Just select three emotional states that would describe how you feel right now
-# 
-""")
-
-# your_emotion = st.selectbox(
-#      'How do you feel right now?',
-#      emotions_list
-#         )
-
-options = st.multiselect(
-   'how do you feel?',
-   emotions_list,
-   emotions_list[:2]
-   )
-#st.write('You selected:', your_emotion)
-st.markdown("""
-##
-""")
-
-st.write(f'i see you feel **{options[0]}**, **{options[1]}** and **{options[2]}**')
-
-st.markdown("""
-##
-""")
-
-
-if st.button('Create music from selected emotions!'):
-    st.balloons()
-
-if st.button('Play music from selected emotions!'):
-    audio_file = open('chopin.ogg', 'rb')
-    audio_bytes = audio_file.read()
-    st.audio(audio_bytes, format='audio/ogg')
-
 
