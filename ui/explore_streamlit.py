72e48a07aa5cd996d38f4f49d71f30b14cce3940
Daniil Volf <danjavolf@gmail.com>
2021-06-04 11:39:30 +0200

    converter integration

--- ui/explore_streamlit.py
+++ ui/explore_streamlit.py
@@ -1,21 +1,19 @@
 import streamlit as st
-from MusicWithEmotions.runprograma import Runprograma
-from MusicWithEmotions.facerecognition import Facerecognition
-from MusicWithEmotions.musicgeneration import Musicgeneration
-from MusicWithEmotions.encoderemotion import Encoderemotion
-import altair as alt
+# from MusicWithEmotions.runprograma import Runprograma
+# from MusicWithEmotions.facerecognition import Facerecognition
+# from MusicWithEmotions.musicgeneration import Musicgeneration
+# from MusicWithEmotions.encoderemotion import Encoderemotion
 
 from midi2audio import FluidSynth 
 
 from PIL import Image
 import numpy as np
 import pandas as pd
-from numpy import random
 
 
 
-#image = Image.open('ui/mwe_logo.PNG')
-#st.image(image, use_column_width=True)
+image = Image.open('ui/mwe_logo.PNG')
+st.image(image, use_column_width=True)
 
 
 
@@ -53,10 +51,7 @@ if picture:
     midi = test_.run()
     st.write(type(midi))
     st.write(midi.lower())
-    st.write("Testing...")
-    audio_file_mid = open('ui/test.mid', 'rb')
-    audio_bytes_mid = audio_file_mid.read()
-    st.audio(audio_bytes_mid, format='audio/ogg', start_time=0)    
+ 
 
 
 if submit:
@@ -65,13 +60,20 @@ if submit:
     #st.write(run_program.run())
     #st.image(run_program.picture, caption='Your Image.', width=100,  use_column_width=False)
 
-    st.write("here we play wav")
-    audio_file_wav = open('ui/night_piano.wav', 'rb')
+    midi_file_path = f"ui/midi/{midi}"
+    wave_file_path = midi_file_path.replace(".mid",".wav")
+
+    fs = FluidSynth()
+    fs.midi_to_audio(midi_file_path, wave_file_path)
+
+    st.write("here we test the easy way")
+    st.audio(wave_file_path, format='audio/ogg', start_time=0)
+    st.markdown("   You like the song? Download it by right-clicking on the player")
+
+    st.write("here we test the way with the opener")
+    audio_file_wav = open(wave_file_path, 'rb')
     audio_bytes_wav = audio_file_wav.read()
     st.audio(audio_bytes_wav, format='audio/wav', start_time=0)
-
-    st.write("here we play mid")
-    audio_file_mid = open('ui/test_sound.mid', 'rb')
-    audio_bytes_mid = audio_file_mid.read()
-    st.audio(audio_bytes_mid, format='audio/ogg', start_time=0)
     
+
+
