457b9d9a02bcecb3a0c792e9527a034e0eaf0feb
Daniil Volf <danjavolf@gmail.com>
2021-06-07 13:39:07 +0200

    test - delete with button

--- ui/explore_streamlit.py
+++ ui/explore_streamlit.py
@@ -4,25 +4,15 @@ from MusicWithEmotions.facerecognition import Facerecognition
 from MusicWithEmotions.musicgeneration import Musicgeneration
 from MusicWithEmotions.encoderemotion import Encoderemotion
 
-from midi2audio import FluidSynth 
+from midi2audio import FluidSynth
+import os
 
 from PIL import Image
 import numpy as np
 import pandas as pd
 
-
-
 image = Image.open('ui/mwe_logo.PNG')
 st.image(image, use_column_width=True)
-
-
-
-# dataframe that stores potential emotions
-# emotions_df = pd.DataFrame({
-#           "emotion": ["happy", "sad", "scared", "angry", "neutral", "surprised", "disgusted"]
-#         })
-
-#emotions_list = ["happy", "sad", "scared", "angry", "neutral", "surprised", "disgusted"]
 
 st.markdown("""
 ## Lets create some music
@@ -31,8 +21,6 @@ st.markdown("""
 ###
 """)
 
-
-
 # And within an expander
 
 form = st.form(key='my-form')
@@ -40,20 +28,17 @@ picture = form.file_uploader("your image", type="jpg")
 #name = form.text_input('Enter your name')
 submit = form.form_submit_button('Create music with emotions!')
 
-
 if picture:
 
     image = Image.open(picture)
     img = np.array(image)
-    st.image(image, caption='Your Image.', width=300,  use_column_width=False)
+    st.image(image, caption='Your Image.', width=300, use_column_width=False)
     test_ = Runprograma(img)
     midi, emotion = test_.run()
     st.write('')
     st.write(f"Detected emotion: {emotion}")
     st.write('')
     #st.write(midi.lower())
- 
-
 
 if submit:
 
@@ -62,15 +47,14 @@ if submit:
     #st.image(run_program.picture, caption='Your Image.', width=100,  use_column_width=False)
 
     midi_file_path = f"ui/midi/{midi}"
-    wave_file_path = midi_file_path.replace(".mid",".wav")
+    wave_file_path = midi_file_path.replace(".mid", ".wav")
 
     fs = FluidSynth()
     fs.midi_to_audio(midi_file_path, wave_file_path)
 
     st.audio(wave_file_path, format='audio/ogg', start_time=0)
-    st.markdown("   You like the song? Download it by right-clicking on the player")
+    #st.markdown("   You like the song? Download it by right-clicking on the player")
 
-    #st.write("here we test the way with the opener")
-    #audio_file_wav = open(wave_file_path, 'rb')
-    #audio_bytes_wav = audio_file_wav.read()
-    #st.audio(audio_bytes_wav, format='audio/wav', start_time=0)
+    if st.button('Delete generated files'):
+        os.remove(midi_file_path)
+        os.remove(wave_file_path)
