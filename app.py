61ab20fd3b2323762cefe030ca1f33ea59a439b1
Daniil Volf <danjavolf@gmail.com>
2021-06-09 22:54:18 +0200

    team webpage

--- app.py
+++ app.py
@@ -10,6 +10,26 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 
+# def img_to_bytes(img_path):
+#     img_bytes = Path(img_path).read_bytes()
+#     encoded = base64.b64encode(img_bytes).decode()
+#     return encoded
+
+# header_html = "<img src='data:image/png;base64,{}' class='img-fluid'>".format(
+#     img_to_bytes("header.png")
+# )
+# st.markdown(
+#     header_html, unsafe_allow_html=True,
+# )
+
+# with open("style.css") as f:
+#     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+# path = "..."
+# image = Image.open(path)
+
+# st.image(image, width = 150)
+
 webpage = st.sidebar.radio("Navigation",
                            ["Music generator", "How it works", "Team"])
 
@@ -70,40 +90,59 @@ elif webpage == 'How it works':
 
 elif webpage == 'Team':
     st.header("Project Team")
-    col1, mid, col2 = st.beta_columns([1, 1, 20])
-    image = Image.open('image/juan.jpg')
+    st.write('')
+    st.write('')
 
-    with col1:
-        st.image(image, use_column_width=100)
-    with col2:
-        st.write('A Name')
+    st.markdown("### Juan Carlos Mayoral Molina")
+    image_1 = Image.open('image/juan.jpg')
+    text_1 = """I find extremely interesting the possibilities that Data Science 
+    offers to relate areas such as: programming, statistics, a lgebra, probability, 
+    among others, which for me are of great interest. And that through techniques 
+    of analysis of large amounts of data we can find relationships that allow us 
+    to see these large amounts of data from different perspectives and thus help us 
+    to obtain new information. I am very interested in the topic of artificial 
+    intelligence, its areas of study and application. I intend to learn more about some of 
+    these areas such as Machine Learning and Deep Learning. I am interested in working on 
+    a project to apply this knowledge, so I will find a job after this course."""
+    st.image(image_1, width=170, use_column_width=False)
+    st.write(text_1)
+    st.write("")
 
-    # image = Image.open('image/juan.jpg')
-    # st.image(image, use_column_width=100)
+    st.markdown("### Georgios-Alexandros Christodoulou")
+    image_2 = Image.open('image/georgios.jpg')
+    text_2 = """I find the whole concept of Data Science fascinating, i allways liked 
+    Statistics so that went well... I don't have a background in Data Science or 
+    Web Dev, i studied Agriculture in University, though i never finished my studies. 
+    I did a lot of statistics there, so here goes that...If i remember something else i will update :)"""
+    st.image(image_2, width=170, use_column_width=False)
+    st.write(text_2)
+    st.write("")
 
-    text """I find extremely interesting the possibilities that Data Science offers to relate areas such as: programming, statistics, a
-    lgebra, probability, among others, which for me are of great interest. And that through techniques of analysis of large amounts of 
-    data we can find relationships that allow us to see these large amounts of data from different perspectives and thus help us to obtain new information.
+    st.markdown("### Simone Totino")
+    image_3 = Image.open('image/totino.jpg')
+    text_3 = """Management consultant (energy business) currently on a sabbatical year. 
+    Decided to cure my digital illiteracy and very curious to discover where this BootCamp 
+    will lead me… Committed to apply my current and future skills to have a more sustainable 
+    impact. Passionate about travelling, enthusiastic about outdoor activities, in love with the sea."""
+    st.image(image_3, width=170, use_column_width=False)
+    st.write(text_3)
+    st.write("")
 
-I am very interested in the topic of artificial intelligence, its areas of study and application. I intend to learn more about some of 
-these areas such as Machine Learning and Deep Learning.
+    st.markdown("### Daniil Volf")
+    image_4 = Image.open('image/danja.jpg')
+    text_4 = """Hi Im Daniel, my background is innovation management and engineering. Usually my role in 
+    IT projects was project or product manager and now i want to dive deeper into programming and Data Science!"""
+    st.image(image_4, width=170, use_column_width=False)
+    st.write(text_4)
+    st.write("")
 
-I am interested in working on a project to apply this knowledge, so I will find a job after this course."""
+# col1, col2, col3 = st.beta_columns([1,6,1])
 
-    # image = Image.open('image/georgios.jpg')
-    # st.image(image, use_column_width=True)
+# with col1:
+# st.write("")
 
-    text = """I find the whole concept of Data Science fascinating, i allways liked Statistics so that went well... 
-    I don't have a background in Data Science or Web Dev, i studied Agriculture in University, though i never finished my studies. 
-    I did a lot of statistics there, so here goes that...If i remember something else i will update :) """
+# with col2:
+# st.image("https://i.imgflip.com/amucx.jpg")
 
-    # image = Image.open('image/totino.jpg')
-    # st.image(image, use_column_width=True)
-    text = """Management consultant (energy business) currently on a sabbatical year. Decided to cure my digital 
-    illiteracy and very curious to discover where this BootCamp will lead me… Committed to apply my current 
-    and future skills to have a more sustainable impact.
-Passionate about travelling, enthusiastic about outdoor activities, in love with the sea."""
-
-    # image = Image.open('image/danja.jpg')
-    # st.image(image, use_column_width=True)
-    text = """Hi Im Daniel, my background is innovation management and engineering. Usually my role in IT projects was project or product manager and now i want to dive deeper into programming and Data Science!"""
+# with col3:
+# st.write("")
