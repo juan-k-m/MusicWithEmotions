f363326d996d523ba19b0144decd82a534a44df4
Daniil Volf <danjavolf@gmail.com>
2021-06-09 18:56:43 +0200

    team pictures

--- app.py
+++ app.py
@@ -69,14 +69,41 @@ elif webpage == 'How it works':
     st.image(image, use_column_width=True)
 
 elif webpage == 'Team':
+    st.header("Project Team")
+    col1, mid, col2 = st.beta_columns([1, 1, 20])
     image = Image.open('image/juan.jpg')
-    st.image(image, use_column_width=True)
 
-    image = Image.open('image/georgios.jpg')
-    st.image(image, use_column_width=True)
+    with col1:
+        st.image(image, use_column_width=100)
+    with col2:
+        st.write('A Name')
 
-    image = Image.open('image/totino.jpg')
-    st.image(image, use_column_width=True)
+    # image = Image.open('image/juan.jpg')
+    # st.image(image, use_column_width=100)
 
-    image = Image.open('image/danja.jpg')
-    st.image(image, use_column_width=True)
+    text """I find extremely interesting the possibilities that Data Science offers to relate areas such as: programming, statistics, a
+    lgebra, probability, among others, which for me are of great interest. And that through techniques of analysis of large amounts of 
+    data we can find relationships that allow us to see these large amounts of data from different perspectives and thus help us to obtain new information.
+
+I am very interested in the topic of artificial intelligence, its areas of study and application. I intend to learn more about some of 
+these areas such as Machine Learning and Deep Learning.
+
+I am interested in working on a project to apply this knowledge, so I will find a job after this course."""
+
+    # image = Image.open('image/georgios.jpg')
+    # st.image(image, use_column_width=True)
+
+    text = """I find the whole concept of Data Science fascinating, i allways liked Statistics so that went well... 
+    I don't have a background in Data Science or Web Dev, i studied Agriculture in University, though i never finished my studies. 
+    I did a lot of statistics there, so here goes that...If i remember something else i will update :) """
+
+    # image = Image.open('image/totino.jpg')
+    # st.image(image, use_column_width=True)
+    text = """Management consultant (energy business) currently on a sabbatical year. Decided to cure my digital 
+    illiteracy and very curious to discover where this BootCamp will lead me… Committed to apply my current 
+    and future skills to have a more sustainable impact.
+Passionate about travelling, enthusiastic about outdoor activities, in love with the sea."""
+
+    # image = Image.open('image/danja.jpg')
+    # st.image(image, use_column_width=True)
+    text = """Hi Im Daniel, my background is innovation management and engineering. Usually my role in IT projects was project or product manager and now i want to dive deeper into programming and Data Science!"""
