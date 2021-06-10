9c4fd0aef448a5d20dde952a0b1df2c162297f0e
Juan Mayoral <mayoral@juank.de>
2021-06-10 09:21:55 +0200

    making work the another magenta model...

--- MusicWithEmotions/services/magentaimprov.py
+++ MusicWithEmotions/services/magentaimprov.py
@@ -3,6 +3,12 @@ from magenta.models.improv_rnn import improv_rnn_sequence_generator
 from magenta.models.shared import sequence_generator_bundle
 from note_seq.protobuf import generator_pb2
 from note_seq.protobuf import music_pb2
+
+from magenta.models.shared import sequence_generator
+from magenta.pipelines import chord_pipelines
+from magenta.pipelines import melody_pipelines
+
+
 from MusicWithEmotions.services.sequencemaker import Sequencemaker
 import datetime
 import note_seq
@@ -34,8 +40,9 @@ class Magentaimprov:
 
 
     def generate_music(self):
-        input_sequence = self.sequencemaker.create('happy') # 
-        
+        self.create_sequence_testing()
+        input_sequence = self.initialnotes # 
+        print(input_sequence)
         num_steps = 80 #was 128 change this for shorter or longer sequences
         temperature = 1.2
         # Set the start time to begin on the next step after the last note ends.
@@ -44,9 +51,13 @@ class Magentaimprov:
         qpm = input_sequence.tempos[0].qpm 
         seconds_per_step = 60.0 / qpm / self.model.steps_per_quarter
         total_seconds = num_steps * seconds_per_step
-
+        print(last_end_time + seconds_per_step)
         generator_options = generator_pb2.GeneratorOptions()
         generator_options.args['temperature'].float_value = temperature
+        #generator_options.args['backing_chords'].string_value = 'C Am'
+        #generator_options.args['primer_melody'].int_value = 60#[60, -2, 60, -2, 67, -2, 67, -2]
+        #generate_section = generator_options.generate_sections.add(start_time=0, end_time=20)
+        generate_section = generator_options.generate_sections.add(backing_chords='C Am', primer_melody=[60, -2, 60, -2, 67, -2, 67, -2])
         generate_section = generator_options.generate_sections.add(
           start_time=last_end_time + seconds_per_step,
           end_time=total_seconds)
@@ -58,24 +69,31 @@ class Magentaimprov:
     def load_model(self):
         root_dir = self.get_root_dir()
         model_location = os.path.join(root_dir,"MusicWithEmotions","services","magmodels","chord_pitches_improv.mag")
-        
-        print('Here->')
-        #bundle = sequence_generator_bundle.read_bundle_file('services/magmodels/basic_rnn.mag')
-        #bundle = sequence_generator_bundle.read_bundle_file(model_location)
-        #generator_map = improv_rnn_sequence_generator.get_generator_map()
-        #print(generator_map.__dict__)
-        #melody_rnn = generator_map['attention_rnn'](checkpoint=None, bundle=bundle)
-        #melody_rnn.initialize()
-        #self.model = melody_rnn
+    
+        bundle = sequence_generator_bundle.read_bundle_file(model_location)
+        generator_map = improv_rnn_sequence_generator.get_generator_map()
+        melody_rnn = generator_map['chord_pitches_improv'](checkpoint=None, bundle=bundle)
+        melody_rnn.initialize()
+        self.model = melody_rnn
     
     def create_sequence_testing(self, basicnotes=None):
         twinkle_twinkle = music_pb2.NoteSequence()
-        # Add the notes to the sequence.
-        twinkle_twinkle.notes.add(pitch=60, start_time=0.0, end_time=0.5, velocity=90)
-        twinkle_twinkle.notes.add(pitch=70, start_time=0.5, end_time=1.0, velocity=90)
-        twinkle_twinkle.notes.add(pitch=80, start_time=1.0, end_time=1.5, velocity=90)
-        twinkle_twinkle.notes.add(pitch=90, start_time=1.5, end_time=2.0, velocity=90)
+        twinkle_twinkle.notes.add(pitch=60, start_time=0.0, end_time=0.5, velocity=80)
+        twinkle_twinkle.notes.add(pitch=60, start_time=0.5, end_time=1.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=67, start_time=1.0, end_time=1.5, velocity=80)
+        twinkle_twinkle.notes.add(pitch=67, start_time=1.5, end_time=2.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=69, start_time=2.0, end_time=2.5, velocity=80)
+        twinkle_twinkle.notes.add(pitch=69, start_time=2.5, end_time=3.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=67, start_time=3.0, end_time=4.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=65, start_time=4.0, end_time=4.5, velocity=80)
+        twinkle_twinkle.notes.add(pitch=65, start_time=4.5, end_time=5.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=64, start_time=5.0, end_time=5.5, velocity=80)
+        twinkle_twinkle.notes.add(pitch=64, start_time=5.5, end_time=6.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=62, start_time=6.0, end_time=6.5, velocity=80)
+        twinkle_twinkle.notes.add(pitch=62, start_time=6.5, end_time=7.0, velocity=80)
+        twinkle_twinkle.notes.add(pitch=60, start_time=7.0, end_time=8.0, velocity=80) 
         twinkle_twinkle.total_time = 8
+
         twinkle_twinkle.tempos.add(qpm=60)
         self.initialnotes =  twinkle_twinkle
 
@@ -124,6 +142,7 @@ if __name__ == '__main__':
     #print(type(test.create_sequence()))
     #seq = test.create_sequence()
     midi = test.load_model()
+    test.generate_music()
     print(midi)
 
 
